fbd6b6a7a098eab7606d15b956493bf82afddca8
anna <03ayv21@gmail.com>
2025-05-06 15:13:22 +0200

    add community page

--- Angular-TracShot/src/app/app.component.ts
+++ Angular-TracShot/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FeaturesComponent } from './features/features.component';
+import { CommunityComponent } from './community/community.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FeaturesComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FeaturesComponent, CommunityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
