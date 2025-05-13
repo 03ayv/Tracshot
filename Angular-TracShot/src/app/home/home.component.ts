c68250c3d92f67d1cf5cf4ee1c010360863f31a6
anna <03ayv21@gmail.com>
2025-05-13 22:58:58 +0200

    add automatic scroll to center target

--- Angular-TracShot/src/app/home/home.component.ts
+++ Angular-TracShot/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TargetComponent } from "../target/target.component";
 
@@ -9,6 +9,50 @@ import { TargetComponent } from "../target/target.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('targetContainer') targetContainer!: ElementRef;
+  private isScrolling = false;
+  private lastScrollPosition = 0;
 
+  ngAfterViewInit() {
+    // Initial check for target position
+    this.checkTargetPosition();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    if (this.isScrolling) return;
+    
+    const currentScrollPosition = window.scrollY;
+    const isScrollingDown = currentScrollPosition > this.lastScrollPosition;
+    
+    if (isScrollingDown) {
+      this.checkTargetPosition();
+    }
+    
+    this.lastScrollPosition = currentScrollPosition;
+  }
+
+  private checkTargetPosition() {
+    const targetElement = this.targetContainer.nativeElement;
+    const rect = targetElement.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    
+    // If target is partially visible or just below viewport
+    if (rect.top > windowHeight * 0.3 && rect.top < windowHeight * 1.5) {
+      this.isScrolling = true;
+      
+      // Calculate position to center the target
+      const targetCenter = targetElement.offsetTop - (windowHeight - targetElement.offsetHeight + 65) / 2;
+      
+      window.scrollTo({
+        top: targetCenter,
+        behavior: 'smooth'
+      });
+
+      setTimeout(() => {
+        this.isScrolling = false;
+      }, 1000);
+    }
+  }
 }
