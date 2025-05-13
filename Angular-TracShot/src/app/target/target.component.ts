62da7a655fe42e12437d6a48d0658d0faa4b7ac8
anna <03ayv21@gmail.com>
2025-05-13 14:22:42 +0200

    change layout to dark

---
+++ Angular-TracShot/src/app/target/target.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-target',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './target.component.html',
+  styleUrl: './target.component.css'
+})
+export class TargetComponent {
+}
